ba72db4b7f3005c4e05bc593d1bf3335f55d1883
Juan-Carlos-Mora <113145392+Juan-Carlos-Mora@users.noreply.github.com>
2023-03-20 00:23:19 -0300

    Falta algunos detalles esteticos

--- src/components/Context/Cart.jsx
+++ src/components/Context/Cart.jsx
@@ -2,11 +2,13 @@ import { useContext, useState } from "react";
 import { Link } from "react-router-dom";
 import { CartContext } from "./CartContext";
 import trash from "../iconos/icons8-borrar-para-siempre-100.png";
+import { addDoc, collection,  getFirestore} from "firebase/firestore"; 
 
 const Cart = () => {
     const [nombre, setNombre] = useState("");
     const [email, setEmail] = useState("");
     const [telefono, setTelefono] = useState("");
+    const [orderId, setOrdeId] = useState("");
     const {cart, cartTotal,clear, removeItem, cartSum} = useContext(CartContext);
 
 
@@ -15,6 +17,11 @@ const Cart = () => {
         const fecha = new Date();
         const date = `${fecha.getFullYear()}-${fecha.getMonth + 1}-${fecha.getDate()} ${fecha.getHours()}:${fecha.getMinutes()}:${fecha.getSeconds()}}`;
         const order = {buyer, items:cart, date:date,total:cartTotal()}
+
+        const db = getFirestore();
+        const ordersCollection = collection ( db, "orders");
+        addDoc(ordersCollection, order).then(data => { setOrdeId(data.id);});
+        clear();
     }
 if(cartTotal() === 0) {
     return (
@@ -35,29 +42,30 @@ if(cartTotal() === 0) {
 }
 
     return (
-        <div className="container">
+        <div className="container-fluid">
         <div className="row">
-            <div className="col-md-4">
+                        <h1 className="text-center">Skin Seleccionadas</h1>
+            <div className="col-md-5">
                 <form>
                     <div class="mb-3">
-                        <label for="nombre" class="form-label">Nombre</label>
+                        <label htmlFor="nombre" class="form-label">Nombre</label>
                         <input type="text" class="form-control" id="nombre" onInput={(e) =>{
                             setNombre(e.target.value);}}/>
                     </div>
                     <div class="mb-3">
-                        <label for="Email" class="form-label">Correo Electronico</label>
+                        <label htmlFor="Email" class="form-label">Correo Electronico</label>
                         <input type="email" class="form-control" id="Correo electronico"onInput={(e) =>{
                             setEmail(e.target.value);}}/>
                     </div>
                     <div class="mb-3 form-check">
-                        <label class="Telefono " className="form-label">Numero de Telefono</label>
+                        <label htmlFor="Telefono " className="form-label">Numero de Telefono</label>
                         <input type="number" class="form-control" id="numero telefono"onInput={(e) =>{
                             setTelefono(e.target.value);}}/>
                     </div>
-                    <button type="submit" class="btn btn-primary">Generar Orden</button>
+                    <button type="submit" class="btn btn-primary" onClick={generarOrden}>Generar Orden</button>
                 </form>
-            <div className="col-md-8">
-                <h1 className="text-center">Skin Seleccionadas</h1>
+                </div>
+            <div className="col-md-7">
                 <table className="table colores">
                     <tr>
                         <td className="text-end" colSpan={5}><Link type="Button" className="btn Botonagregar"  onClick={() => {clear()}}>Vaciar Carrito</Link></td>
@@ -83,7 +91,12 @@ if(cartTotal() === 0) {
                 </table>
             </div>
         </div>
-    </div>
+            <div className="row">
+            <div className="col-md-4">
+                {orderId ? <div className="alert alert-warning" role={"alert"}><h3>Gracias por Su Compra</h3> <p>Se genero una orden de compra con el ID: <b>{orderId}</b></p></div>:""}
+
+            </div>
+     </div>
     </div>
     )
 }
